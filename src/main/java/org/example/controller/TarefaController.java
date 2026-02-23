e45fc997998ed80e5d63b21708e8c470c883084e
eugigizinea <giovanna.dkopke@gmail.com>
2026-02-23 19:48:17 -0300

    atualizando consultas

--- src/main/java/org/example/controller/TarefaController.java
+++ src/main/java/org/example/controller/TarefaController.java
@@ -3,13 +3,14 @@ package org.example.controller;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.example.dto.TarefaDTO;
+import org.example.entity.TarefaEntity;
 import org.example.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.ServerRequest;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping ("api/tarefas")
@@ -21,4 +22,18 @@ public class TarefaController {
     public void registrarAtividade ( @RequestBody TarefaDTO request){
     tarefaService.save(request);
     }
+    @GetMapping
+    public List<TarefaEntity> consultar (){
+        return tarefaService.consultar();
+    }
+    @GetMapping("{id}")
+    public TarefaEntity consultar (@PathVariable UUID id){
+        return tarefaService.consultar(id);
+    }
+    @PatchMapping("{id}")
+    public TarefaEntity atualizar(@PathVariable UUID id,
+                                  @RequestBody TarefaDTO request){
+        return tarefaService.atualizar(id, request);}
+
+
 }
